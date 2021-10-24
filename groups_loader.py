25fe2abe859cff5b516db2fc780403325c83ea3c
Uladzislau Khamkou <kham@tuta.io>
2021-10-24 19:36:52 +0300

    groups_loader: support points from task.cfg

--- groups_loader.py
+++ groups_loader.py
@@ -25,13 +25,38 @@ tests = tree.getroot().findall('.//testset[@name=\'tests\']/tests')[0]
 dic = {}
 
 def add(group, points):
+    # NEW CODE: add support for points=None
     if not dic.get(group):
-        dic[group] = [float(0), 0]
-    dic[group][0] += float(points)
-    dic[group][1] += 1
+        dic[group] = [float(points) if points is not None else None, 1]
+    else:
+        if points is not None:
+            dic[group][0] += float(points)
+        dic[group][1] += 1
 
-for i in tests:
-    add(i.attrib['group'], i.attrib['points'])
+for test in tests:
+    #add(i.attrib['group'], i.attrib['points'])
+    # use group=0 if there is no word "group" in xml file
+    group = test.attrib['group'] if "group" in test.attrib else "0"
+    # use points=None if there is no word "points" in xml file
+    points = test.attrib['points'] if 'points' in test.attrib else None
+    add(group, points)
+
+
+
+# read points from "task.cfg"
+# format: GROUP1_POINTS = points_for_group_1 points_for_group_2 ...
+XML_IS_OLD = any(group_info[0] is None for group_info in dic.values())
+if XML_IS_OLD:
+    with open('task.cfg') as cfg:
+        cfg = cfg.read().split('\n')
+        GROUP1_POINTS = next(line for line in cfg if line.upper().startswith('GROUP1_POINTS'))
+        points_for_groups = [0] + GROUP1_POINTS.split('=')[1].strip().split(' ')
+    for group, group_info in dic.items():
+        if group_info[0] is None:
+            group_info[0] = float(points_for_groups.pop(0))
+
+
+
 
 total = float(0)
 for group, (points, tests) in dic.items():
