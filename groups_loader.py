caf27ffc8aef0e9b1506631175a129313d4fa7d3
Uladzislau Khamkou <kham@tuta.io>
2021-10-26 22:03:19 +0300

    groups_loader: refactoring

--- groups_loader.py
+++ groups_loader.py
@@ -1,115 +1,57 @@
 import xml.etree.ElementTree as ET
+from collections import namedtuple, defaultdict
 import os
 
-problem = "problem.xml"
+# g2i converts group name to group number
+g2i = lambda g: 0 if g == "samples" or g is None else int(g.split(".")[0])
+Group = namedtuple("Group", "points dependencies tests")
+Test = namedtuple("Test", "index points")
 
 
 def read_groups_from_testset_with_tests(testset):
-    # g2i converts group name to group number
-    def g2i(group_name):
-        if group_name == "samples":
-            return 0
-        return int(group_name)
+    tests, groups = testset.find("./tests"), testset.find("./groups") or []
+    tests_info = [(g2i(t.attrib.get("group")), t) for t in tests]
+    groups_info, groups = {g2i(g.attrib["name"]): g for g in groups}, {}
+    for test_index, (group, test_info) in enumerate(tests_info):
+        if group not in groups:
+            deps = {d for d in groups_info[group] if d.tag == "dependencies"}
+            deps = {g2i(d.attrib["group"]) for ds in deps for d in ds}
+            each = groups_info[group].attrib["points-policy"] == "each-test"
+            policy = "sum" if each else "all"
+            groups[group] = Group(points=policy, dependencies=deps, tests=[])
+        points = test_info.attrib.get("points")
+        points = int(float(points)) if points else None
+        groups[group].tests.append(Test(test_index, points))
+    for i, (points, _, tests) in groups.items():
+        if points == "all":
+            if all(p is not None for (_, p) in tests):
+                points = sum(p for (_, p) in tests)
+            groups[i] = groups[i]._replace(points=points)
+    return groups
 
-    tests = testset.find("./tests")
-    groups = testset.find("./groups") or []
-
-    # define tests_info = [(group_number, points_per_test)]
-    tests = [(t.attrib.get("group", 0), t.attrib.get("points")) for t in tests]
-    tests_info = [(g2i(g), p) for g, p in tests]
-
-    # define groups_points_policy = {group_number: points-policy}
-    gs = {g.attrib["name"]: g.attrib["points-policy"] for g in groups}
-    groups_points_policy = {g2i(g): v for g, v in gs.items()}
-
-    # define groups_dependencies = {group_number: dependencies}
-    groups_dependencies = dict()
-    for g in groups:
-        group_name = g.attrib["name"]
-        group_number = 0 if group_name == "samples" else int(group_name)
-        groups_dependencies[group_number] = set()
-        for dependencies in (
-            child for child in g if child.tag == "dependencies"
-        ):
-            for dependency in dependencies:
-                dependency_group_name = dependency.attrib["group"]
-                dependency_group_number = (
-                    0
-                    if dependency_group_name == "samples"
-                    else int(dependency_group_name)
-                )
-                groups_dependencies[group_number].add(dependency_group_number)
-
-    # find tests for every group
-    dic = {}
-    for test_ind, (test_group, test_points) in enumerate(tests_info):
-        if test_group not in dic:
-            dic[test_group] = set()
-        dic[test_group].add(test_ind)
-
-    # regroup using groups points policy
-    new_dic, o2n = {}, {}
-    for group, tests in dic.items():
-        if (
-            group > 0
-            and group in groups_points_policy
-            and groups_points_policy[group] == "each-test"
-        ):
-            for test in tests:
-                o2n.setdefault(group, set()).add(len(new_dic))
-                new_dic[len(new_dic)] = {test}
-        else:
-            o2n[group] = {len(new_dic)}
-            new_dic[len(new_dic)] = tests
-    dic = new_dic
-
-    # recalculate group dependencies
-    new_deps = {}
-    for old_group, deps in groups_dependencies.items():
-        for group in o2n[old_group]:
-            new_deps[group] = {g for d in deps for g in o2n[d]}
-    groups_dependencies = new_deps
-
-    # calculate points for every group
-    new_dic = {}
-    for group, tests in dic.items():
-        points = (
-            sum(float(tests_info[t][1]) for t in tests)
-            if all(tests_info[t][1] is not None for t in tests)
-            else None
-        )
-        new_dic[group] = [points, len(tests)]
-    dic = new_dic
-    return (dic, groups_dependencies)
 
 def read_groups_from_testset_with_groups(testset):
-    # g2i converts group name to group number
-    def g2i(group_name):
-        if group_name.lower() in ("samples", "0. sample tests"):
-            return 0
-        return int(group_name)
+    groups_info, groups = {g2i(g.attrib["comment"]): g for g in testset}, {}
+    for i, group in groups_info.items():
+        points = int(group.attrib["group-bonus"])
+        tests = [Test(i, t.attrib.get("points")) for i, t in enumerate(group)]
+        tests = [Test(i, int(p) if p else None) for i, p in tests]
+        deps = set(map(int, group.attrib["require-groups"].split()))
+        groups[i] = Group(points, (deps | {0}) - {i}, tests)
+    return groups
 
-    groups, groups_dependencies = {}, {}
-    for group_info in testset:
-        i = g2i(group_info.attrib['comment'])
-        points = int(group_info.attrib['group-bonus'])
-        deps = set(map(int, group_info.attrib['require-groups'].split()))
-        groups[i] = (points, len(group_info))
-        groups_dependencies[i] = (deps | {0}) - {i}
-    return groups, groups_dependencies
 
 def read_groups_xml(filename):
     if not os.path.isfile(filename):
         return None
     xml_tree = ET.parse(filename)
-    testsets = xml_tree.getroot().findall(".//testset[@name='tests']")
-    if testsets:
-        return read_groups_from_testset_with_tests(testsets[0])
-    testsets = xml_tree.getroot().findall(".//testset[@name='main']")
-    if testsets:
-        return read_groups_from_testset_with_groups(testsets[0])
-    else:
+    tsts = xml_tree.getroot().findall(".//testset[@name='tests']")
+    grps = xml_tree.getroot().findall(".//testset[@name='main']")
+    groups = read_groups_from_testset_with_tests(tsts[0]) if tsts else None
+    groups = read_groups_from_testset_with_groups(grps[0]) if grps else groups
+    if groups is None:
         raise Exception("error: no testset with tests was found")
+    return groups
 
 
 def read_groups_cfg(filename):
@@ -122,70 +64,64 @@ def read_groups_cfg(filename):
     if not os.path.isfile(filename):
         return None
     with open(filename, "rt") as cfg:
-        lines = cfg.read().strip().split("\n")
-
-    # find "<" and ">"
-    info_start = info_end = 0
-    while info_start < len(lines) and lines[info_start].strip() != "<":
-        info_start += 1
-    info_end = info_start + 1
-    while info_end < len(lines) and lines[info_end].strip() != ">":
-        info_end += 1
-    if not (info_start < info_end < len(lines)):
-        return None
-
-    # read groups
-    dic = {}
+        lines = [l.strip() for l in cfg.read().strip().split("\n")]
+    info_start, info_end, groups = lines.index("<"), lines.index(">"), {}
     for i, line in enumerate(lines[(info_start + 1) : info_end]):
-        dic[i] = tuple(map(int, line.strip().split(",")))
-    return (dic, {})
+        points, tests = line.strip().split(",")
+        groups[i] = Group(points, {}, [Test(None, None) for _ in range(tests)])
+    return groups
 
 
-# try to read groups from xml
-dic = read_groups_xml("problem.xml")
-if dic is None:
-    print("problem.xml not found")
-dic = read_groups_xml("problem.xml.polygon") if dic is None else dic
-if dic is None:
-    print("problem.xml.polygon not found")
-# else: read groups from tester.cfg
-dic = read_groups_cfg("tester.cfg") if dic is None else dic
-if dic is None:
-    print("tester.cfg not found")
-
-if dic is None:
+sources = ["problem.xml", "problem.xml.polygon", "tester.cfg"]
+for s in sources:
+    print(f"reading {s}...")
+    groups = read_groups_xml(s) if s.endswith(".xml") else read_groups_cfg(s)
+    if groups:
+        break
+    print(f"{s} not found")
+else:
     print("error reading groups info from xml/cfg files")
     exit(1)
-dic, groups_dependencies = dic
 
 if not os.path.isfile("task.cfg"):
     print("error reading task.cfg: no such file")
     exit(1)
 
+# regroup sum-groups into separate groups
+groups, old_groups, o2n = dict(), groups, defaultdict(set)
+for i, group in old_groups.items():
+    if group.points == "sum":
+        deps = {g for d in group.dependencies for g in o2n[d]}
+        for test in group.tests:
+            o2n[i].add(len(groups))
+            groups[len(groups)] = Group(test.points, deps, [test])
+    else:
+        o2n[i].add(len(groups))
+        groups[len(groups)] = group
+
 # read points from "task.cfg" if some points are None
 # format: GROUP1_POINTS = points_for_group_1 points_for_group_2 points_for_group_3 ...
-if any(group_info[0] is None for group_info in dic.values()):
+if any(group.points is None for group in groups.values()):
     with open("task.cfg") as cfg:
         cfg = cfg.read().split("\n")
-        GROUP1_POINTS = next(
-            line for line in cfg if line.upper().startswith("GROUP1_POINTS")
-        )
-        points_for_groups = [0] + GROUP1_POINTS.split("=")[1].strip().split(" ")
-    for group, group_info in dic.items():
-        if group_info[0] is None:
-            group_info[0] = float(points_for_groups.pop(0))
+        pts = next(l for l in cfg if l.strip().lower() == "group1_points")
+        points_for_groups = [0] + pts.split("=")[1].strip().split(" ")
+    for i, group in groups.items():
+        if group.points is None:
+            groups[i] = group._replace(points=int(points_for_groups.pop(0)))
 
-total = float(0)
+dic = {i: (pts, len(tests)) for i, (pts, _, tests) in groups.items()}
+total = 0
 for group, (points, tests) in dic.items():
     print(f"group #{group}:\t{points} points\t{tests} tests")
     total += points
 print(f"total points: {total}")
 
-if sum(len(deps) for deps in groups_dependencies.values()) > 0:
+if any(len(deps) for (_, deps, _) in groups.values()):
     print("\n -- DEPENDENCIES --")
-    for group, deps in groups_dependencies.items():
-        deps = ", ".join(str(d) for d in deps) if deps else "no dependencies"
-        print(f"group #{group}:\t{deps}")
+    for i, (_, deps, _) in groups.items():
+        deps = " ".join(str(d) for d in deps) if deps else "no dependencies"
+        print(f"group #{i}:\t{deps}")
     print()
 
 if os.path.isfile("task.cfg.old"):
@@ -220,33 +156,19 @@ while dic.get(i):
     i += 1
 lines += lines_end
 
-# find DEPS_BEGIN and DEPS_END in task.cfg
-deps_start = deps_end = 0
-while (
-    deps_start < len(lines)
-    and lines[deps_start].lower().strip() != "deps_begin"
-):
-    deps_start += 1
-if deps_start < len(lines):
-    deps_end = deps_start
-    while (
-        deps_end < len(lines) and lines[deps_end].lower().strip() != "deps_end"
-    ):
-        deps_end += 1
-if not (deps_start < deps_end < len(lines)):
-    deps_start = len(lines)
-    deps_end = deps_start + 1
-    if sum(len(deps) for deps in groups_dependencies.values()) > 0:
-        lines += ["deps_begin\n", "deps_end\n"]
+# update lines from DEPS_BEGIN to DEPS_END in task.cfg
+lines_lower = [l.strip().lower() for l in lines]
+try:
+    deps_beg = lines_lower.index("deps_begin")
+    deps_end = lines_lower.index("deps_end")
+except ValueError:
+    deps_beg, deps_end = len(lines), len(lines) + 1
+    lines += ["deps_begin\n", "deps_end\n"]
 cfg_deps = []
-
-# add dependencies to task.cfg
-if sum(len(deps) for deps in groups_dependencies.values()) > 0:
-    for group, deps in groups_dependencies.items():
-        if deps:
-            deps = " ".join(str(d) for d in deps)
-            cfg_deps.append(f"{group}: {deps}\n")
-    lines[(deps_start + 1) : deps_end] = cfg_deps
+for group, (_, deps, _) in groups.items():
+    if len(deps) > 0:
+        cfg_deps.append(f"{group}: {' '.join(str(d) for d in deps)}\n")
+lines[(deps_beg + 1) : deps_end] = cfg_deps
 
 print("rewriting task.cfg")
 with open("task.cfg", "w") as f:
